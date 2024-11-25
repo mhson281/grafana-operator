edf10fed72d47e8ecd3fb2a7bf1009775eca7751
mhson281 <mhson281@gmail.com>
2024-11-24 22:42:00 -0600

    added a new func for operator to make api call to grafana to add new org

--- internal/controller/grafanaorganization_controller.go
+++ internal/controller/grafanaorganization_controller.go
@@ -17,19 +17,28 @@ limitations under the License.
 package controller
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	grafanav1alpha1 "github.com/mhson281/grafanaOperator/api/v1alpha1"
 )
 
 // GrafanaOrganizationReconciler reconciles a GrafanaOrganization object
 type GrafanaOrganizationReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
 
@@ -37,29 +46,97 @@ type GrafanaOrganizationReconciler struct {
 // +kubebuilder:rbac:groups=grafana.tcodelab.com,resources=grafanaorganizations/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=grafana.tcodelab.com,resources=grafanaorganizations/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the GrafanaOrganization object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *GrafanaOrganizationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := r.Log.WithValues("grafanaorganization", req.NamespacedName)
 
 	var org grafanav1alpha1.GrafanaOrganization
 	if err := r.Get(ctx, req.NamespacedName, &org); err != nil {
+		if errors.IsNotFound(err) {
+			// Resource not found, could have been deleted
+			log.Info("A GrafanaOrganization resource has not been found.")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get GrafanaOrganization")
 		return ctrl.Result{}, err
 	}
 
-	// Update Status
+	if org.Status.OrganizationID != 0 {
+		log.Info("Organization already exists in Grafana", "OrganizationID", org.Status.OrganizationID)
+		return ctrl.Result{}, nil
+	} 
+
+	grafanaOrgID, err := r.createGrafanaOrg(org.Spec.Name)
+	if err != nil {
+		log.Error(err, "Failed to create organization in Grafana")
+		return ctrl.Result{}, err
+	}
+
+	org.Status.OrganizationID = grafanaOrgID
 	if err := r.Status().Update(ctx, &org); err != nil {
+		log.Error(err, "Failed to update GrafanaOrganization status")
 		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *GrafanaOrganizationReconciler) createGrafanaOrg(ctx context.Context, orgName string) (int, error) {
+	apiURL := "http://172.18.0.3:32000/api/orgs"
+
+	var secret corev1.Secret
+	secretName := "grafana"
+	namespace := "grafana"
+
+	if err := r.Client.Get(ctx, client.ObjectKey{Namespace: namespace, Name: secretName}, &secret); err != nil {
+		return 0, fmt.Errorf("Failed to fetch secret %s/%s: %v", namespace, secretName, err)
+	}
+
+	// extract admin username and pass from secret
+	adminName := string(secret.Data["admin-user"])
+	adminPassword := string(secret.Dta["admin-password"])
+
+	payload := map[string]string{"name": orgName}
+	body, _ := json.Marshal(payload)
+
+	// create the http Request
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(body))
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Set("Authorization", "Basic"+basicAuth(adminName, adminPassword))
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the Request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return 0, fmt.Errorf("Failed to create new organization: %s", resp.Status)
+	}
+
+	var result map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return 0, err
+	}
+
+	orgID, ok := result["id"].(float64)
+	if !ok {
+		return 0, fmt.Errorf("Invalid reponse from grafana API")
+	}
+
+	return int(orgID), nil
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
 
 // SetupWithManager sets up the controller with the Manager.
