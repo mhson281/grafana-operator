fc2f59405b128878ff7e1c7554e4c70541102dc0
mhson281 <mhson281@gmail.com>
2024-11-25 20:46:03 -0600

    added grafana team crds and controllers

--- cmd/main.go
+++ cmd/main.go
@@ -151,6 +151,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaOrganization")
 		os.Exit(1)
 	}
+	if err = (&controller.GrafanaTeamReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GrafanaTeam")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
